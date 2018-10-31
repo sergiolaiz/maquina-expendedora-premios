6f998cb153b22f9528d7b47c0c7063b594afa846
Sergio <sergio.laiz87@gmail.com>
2018-10-31 14:09:02 +0100

    vacia todo el dinero de la maquina

--- MaquinaExpendedoraMejorada.java
+++ MaquinaExpendedoraMejorada.java
@@ -85,4 +85,13 @@ public class MaquinaExpendedoraMejorada {
         balanceClienteActual = 0;
         return cantidadDeDineroADevolver;
     } 
+    
+     public int vaciarDineroDeLaMaquina()
+    {
+        int cantidadAVaciar;
+        cantidadAVaciar = balanceClienteActual + totalDineroAcumulado ;
+        balanceClienteActual = 0;
+        totalDineroAcumulado = 0;
+        return cantidadAVaciar;
+    } 
 }
